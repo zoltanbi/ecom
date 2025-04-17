5c3e05de08aec0fc12462376d486ed8dac4507bc
Zoltanbi <zoltanbiro001@gmail.com>
2025-04-17 11:44:50 -0700

    payload integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,12 @@
+import type { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+  ],
+};
